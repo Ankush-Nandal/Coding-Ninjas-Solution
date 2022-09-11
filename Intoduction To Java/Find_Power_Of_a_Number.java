e2a06ac7dfec382d7565d41d8ad4f5a1ad4feac4
Ankush Nandal <95566773+Ankush-Nandal@users.noreply.github.com>
2022-09-12 00:15:56 +0530

    Introduction To JAVA

---
+++ Intoduction To Java/Find_Power_Of_a_Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x = s.nextInt();
+        int p = 1;
+        for (int i = 1; i <= x; i++) {
+            p = p * n;
+        }
+        System.out.println(p);
+    }
+}
+// Ankush-Nandal...
