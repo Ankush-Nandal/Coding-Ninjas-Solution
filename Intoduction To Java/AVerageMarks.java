d29ccd4ec3dee0f073c06e00858f7948884ce7bb
Ankush Nandal <95566773+Ankush-Nandal@users.noreply.github.com>
2022-09-12 00:03:35 +0530

    Introduction To JAVA

--- Intoduction To Java/AVerageMarks.java
+++ Intoduction To Java/AVerageMarks.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-       // System.out.println("Enter the Character not integer");
         char a = s.next().charAt(0);
-      //  System.out.println("Enter 1st Integer");
         int m1 = s.nextInt();
-      //  System.out.println("Enter 2st Integer");
         int m2 = s.nextInt();
-      //  System.out.println("Enter 3rd Integer");
         int m3 = s.nextInt();
         int Average = (m1 + m2 + m3) / 3;
-      //  System.out.println("the Printed Character is => " + a);
          System.out.println(a);
-        //System.out.println("The Average of all three numbers are => " + Average);
          System.out.println(Average);
     }
 }
+//Ankush-Nandal...
